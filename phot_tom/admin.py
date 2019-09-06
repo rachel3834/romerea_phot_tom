8dd86a1616fa3ec2da05723e9bd4715455087085
rachel3834 <rstreet@lco.global>
2019-09-05 17:35:14 -0700

    Restored DB with stars

--- phot_tom/admin.py
+++ phot_tom/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from tom_dataproducts.models import ReducedDatum
+from tom_targets.models import TargetExtra
 
 admin.site.register(ReducedDatum)
+admin.site.register(TargetExtra)
